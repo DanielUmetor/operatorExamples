fa8dd2b40a920792660cd564ef5d27bcb19870e0
DanielUmetor <dnumetor@gmail.com>
2024-05-15 16:06:20 +0200

    operatorExamples

---
+++ script/code.js
@@ -0,0 +1,15 @@
+// Arthmetic operators: +, -, *, % (modulus), /, ** (exponent), ++, --
+let numb1 = 2, numb2 = 4, numb3 = 5
+// console.log(numb1 + numb2 * numb3);
+// console.log(numb3 % numb1);
+// console.log(numb3 ** numb1)
+// Assignment operators: =, += *=, /=, %=, etc...
+// numb2 *= numb1
+// numb2 = numb2 + numb1 
+// console.log(numb2);
+// Comparison operators: ==, <, >, <=, >=, !=, ===
+// console.log(numb1 === numb2) // will check the value and the data type. 
+// console.log(numb3 / numb1 > numb1);
+// Logical operators: &&, ||, !
+console.log(numb2 == numb2 && numb3 < numb2);
+// Type of operators: typeof and instanceof
